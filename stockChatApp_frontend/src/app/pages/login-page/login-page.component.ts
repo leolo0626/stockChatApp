242591bb7635d73563107e7829036560ad38cea0
Leo Lo <leolo0626@gmail.com>
2022-11-13 14:36:40 +0800

    add validators to login and register form

--- stockChatApp_frontend/src/app/pages/login-page/login-page.component.ts
+++ stockChatApp_frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -9,16 +10,56 @@ import { ViewEncapsulation } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  isRegister = false;
+  isRegister: boolean = false;
+  isRegisterFormSubmitted: boolean = false;
 
-  constructor() {}
+  registerForm!: FormGroup;
+  loginForm!: FormGroup;
+
+
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  get lform(): { [key: string]: AbstractControl; }{
+    return this.loginForm.controls;
+  }
+
+  get rform() : { [key: string]: AbstractControl; }{
+    return this.registerForm.controls;
   }
 
   switchTo(page: string) {
-    console.log(page)
     this.isRegister = page === 'register';
+  }
+
+  onRegister() {
+    this.isRegisterFormSubmitted = true;
+    if (this.registerForm.invalid) {
+      console.log("Invalid Register Form");
+      return; 
+    }
+  }
+
+  onLogin() {
+    if (this.loginForm.invalid) {
+      console.log("Invalid Login Form");
+      return;
+    }
   }
 
 }
