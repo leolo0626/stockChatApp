8a03dfdcc12c3e314d96f93369b2de984b2af2a3
leolo <leolo0626@gmail.com>
2022-11-26 14:10:17 +0800

    add themeing feature (in-progress)

--- stockChatApp_frontend/src/app/app.component.ts
+++ stockChatApp_frontend/src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from './model/user';
 import { UserService } from './services/user.service';
+import { ThemeService } from './services/theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'stockChatApp_frontend';
+  isDarkTheme: Observable<boolean>;
   user?: User;
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private themeService: ThemeService) {
     this.userService.user.subscribe(x => {
       this.user = x;
     });
+  }
+
+  ngOnInit() {
+    this.isDarkTheme = this.themeService.isDarkTheme;
   }
 }
--- stockChatApp_frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ stockChatApp_frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ThemeService } from 'src/app/services/theme.service';
 import { UserService } from 'src/app/services/user.service';
+import { Observable, } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
 
-  constructor(private router: Router, private userService: UserService ) { }
+  constructor(private router: Router, private userService: UserService, private themeService: ThemeService) { }
 
   ngOnInit(): void {
-    
+  }
+
+  toggleDarkTheme(checked: boolean) {
+    this.themeService.setDarkTheme(checked);
   }
   
   onLogout() {
